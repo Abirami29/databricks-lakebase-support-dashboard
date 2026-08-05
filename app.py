ab874040390142293a1898a875c0bd4940009a0b
AbiAttly-Which <abi.attly@which.co.uk>
2026-08-05 23:46:30 +0100

    idk anymore

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 from sqlalchemy import create_engine, text
+from databricks.sdk import WorkspaceClient
 
 st.set_page_config(page_title="SyncMetrics Support Core", layout="wide")
 
